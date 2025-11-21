67434218d1a754e9080222ad252e9e8dd6da8d5f
sarasemsem <mejbrisara@gmail.com>
2025-11-21 20:43:20 +0100

    Initial commit

---
+++ back/service-de-livraison-de-colis/src/test/java/com/kouliet/service/de/livraison/de/colis/ServiceDeLivraisonDeColisKoulietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kouliet.service.de.livraison.de.colis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDeLivraisonDeColisKoulietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
